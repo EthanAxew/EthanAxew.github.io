eff9126d8f2f92dd0e2b65943a84e7acc28e170b
EthanAxew <3785672683@qq.com>
2024-05-05 02:08:50 +0800

    Site updated: 2024-05-05 02:08:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/04/如何利用Hexi搭建个人博客？/"];function toRandomPost(){
+var posts=["2024/05/04/如何利用Hexo搭建个人博客(完整版)/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

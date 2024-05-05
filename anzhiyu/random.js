4c8154e2d38c0ae80513d585815758a9fc9d270d
EthanAxew <3785672683@qq.com>
2024-05-05 19:26:16 +0800

    Site updated: 2024-05-05 19:26:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/04/如何利用Hexo搭建个人博客(完整版)/","2024/05/05/如何利用FRP搭建本地内网穿透/"];function toRandomPost(){
+var posts=["2024/05/04/如何利用Hexo搭建个人博客(完整版)/","2024/05/05/如何利用FRP搭建本地内网穿透/","2024/05/05/写点啥好呢/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
